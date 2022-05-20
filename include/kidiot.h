60e724ee47d52b8ebda83d0a11bd0ac0e91d8b6a
xavier <xavier.tonnellier@epitech.eu>
2022-05-20 17:06:47 +0200

    init repo

---
+++ include/kidiot.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2022
+** kidiot.h
+** File description:
+** kidiot
+*/
+
+#ifndef KIDIOT_H
+    #define KIDIOT_H
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include "raylib.h"
+
+#endif
