9fcab15b8a979086bee26f493cfb1db238fd5e16
Dorian Ayoul <dorian.ayoul@epitech.eu>
2022-05-22 17:15:25 +0200

    [ADD] full sprites + start of scaling

--- src/display/draw.c
+++ src/display/draw.c
@@ -7,31 +7,35 @@
 
 #include "includes.h"
 
-void my_draw_color(char cell, Vector2 pos, Vector2 size_rect, Texture2D textures[4])
+void my_draw_color(kidiot_t *kidiot, char cell, Vector2 pos, Vector2 size)
 {
-    if (cell == 'E') {
-        DrawRectangleV(pos, size_rect, GREEN);
-    }
+    if (cell == 'E')
+        DrawTextureRec(kidiot->textures[3],
+        (Rectangle){0 + (int)(GetTime() * 10) % 5 * 52, 0, 52, 39}, pos, RAYWHITE);
     if (cell == '#')
-        DrawRectangleV(pos, size_rect, BLACK);
+        DrawTexturePro(kidiot->textures[2], (Rectangle){950, 0, 73, 58},
+        (Rectangle){pos.x, pos.y, size.x, size.y}, (Vector2){0, 0}, 0, RAYWHITE);
     if (cell == 'C')
-        DrawTextureRec(textures[2], (Rectangle){731, 0, 73, 57}, pos, RAYWHITE);
+        DrawTextureRec(kidiot->textures[2], (Rectangle){731, 0, 73, 58}, pos, RAYWHITE);
     if (cell == 'B')
-        DrawTextureRec(textures[2], (Rectangle){366, 0, 73, 57}, pos, RAYWHITE);
+        DrawTextureRec(kidiot->textures[2], (Rectangle){366, 0, 73, 58}, pos, RAYWHITE);
     if (cell == 'W')
-        DrawTextureRec(textures[2], (Rectangle){585, 0, 73, 57}, pos, RAYWHITE);
+        DrawTextureRec(kidiot->textures[2], (Rectangle){585, 0, 73, 58}, pos, RAYWHITE);
     if (cell == 'F')
-        DrawTextureRec(textures[2], (Rectangle){74, 0, 73, 57}, pos, RAYWHITE);
+        DrawTextureRec(kidiot->textures[2], (Rectangle){74, 0, 73, 58}, pos, RAYWHITE);
     if (cell == 'T')
-        DrawTextureRec(textures[2], (Rectangle){0, 0, 73, 57}, pos, RAYWHITE);
+        DrawTextureRec(kidiot->textures[2], (Rectangle){0, 0, 73, 58}, pos, RAYWHITE);
     if (cell == 'V')
-        DrawTextureRec(textures[2], (Rectangle){439, 0, 73, 57}, pos, RAYWHITE);
+        DrawTextureRec(kidiot->textures[2], (Rectangle){439, 0, 73, 58}, pos, RAYWHITE);
     if (cell == 'Z')
-        DrawTextureRec(textures[2], (Rectangle){804, 0, 73, 57}, pos, RAYWHITE);
+        DrawTextureRec(kidiot->textures[2], (Rectangle){804, 0, 73, 58}, pos, RAYWHITE);
     if (cell == 'O')
-        DrawTextureRec(textures[2], (Rectangle){220, 0, 73, 57}, pos, RAYWHITE);
+        DrawTextureRec(kidiot->textures[2], (Rectangle){220, 0, 73, 58}, pos, RAYWHITE);
     if (cell == 'M')
-        DrawTextureRec(textures[2], (Rectangle){147, 0, 73, 57}, pos, RAYWHITE);
+        DrawTextureRec(kidiot->textures[2], (Rectangle){147, 0, 73, 58}, pos, RAYWHITE);
+    if (cell == 'S')
+        DrawTexturePro(kidiot->textures[2], (Rectangle){1023, 0, 59, 58},
+        (Rectangle){pos.x, pos.y, size.x, size.y}, (Vector2){0, 0}, 0, RAYWHITE);
 }
 
 void draw_baby(kidiot_t *kidiot, Vector2 pos)
@@ -74,54 +78,74 @@ void draw_mom(kidiot_t *kidiot, Vector2 pos)
     DrawTextureRec(kidiot->textures[1], (Rectangle){18, 65, 17, 60}, pos, RAYWHITE);
 }
 
-void draw_second_map(kidiot_t *play, int wd, int hg)
+void draw_second_map(kidiot_t *kidiot, int width, int height)
 {
-    Vector2 pos_s = {(wd / 2), 0};
-    Vector2 size_rect_s =
-        {wd / strlen(play->second_floor[0]) / 2, hg / get_map_size(play->second_floor)};
-    Vector2 baby_play = {play->baby->pos.y * (wd / strlen(play->second_floor[0]) / 2),
-        play->baby->pos.x * (hg / get_map_size(play->second_floor))};
-    Vector2 mom_play = {play->mom->pos.y * (wd / strlen(play->second_floor[0]) / 2),
-        play->mom->pos.x * (hg / get_map_size(play->second_floor))};
+    Vector2 pos = {(width / 2), 0};
+    Vector2 size =
+        {width / strlen(kidiot->second_floor[0]) / 2, height / get_map_size(kidiot->second_floor)};
+    Vector2 baby = {kidiot->baby->pos.y * (width / strlen(kidiot->second_floor[0]) / 2),
+        kidiot->baby->pos.x * (height / get_map_size(kidiot->second_floor))};
+    Vector2 mom = {kidiot->mom->pos.y * (width / strlen(kidiot->second_floor[0]) / 2),
+        kidiot->mom->pos.x * (height / get_map_size(kidiot->second_floor))};
 
-    for (size_t x  = 0; play->second_floor[x] != NULL; x++) {
-        for (size_t y = 0; play->second_floor[x][y] != '\0'; y++) {
-            my_draw_color(play->second_floor[x][y], pos_s, size_rect_s, play->textures);
-            pos_s.x += wd / strlen(play->second_floor[x]) / 2;
+    for (size_t x  = 0; kidiot->second_floor[x] != NULL; x++) {
+        for (size_t y = 0; kidiot->second_floor[x][y] != '\0'; y++) {
+            DrawTexturePro(kidiot->textures[2], (Rectangle){877, 0, 73, 58},
+            (Rectangle){pos.x, pos.y, size.x, size.y}, (Vector2){0, 0}, 0, RAYWHITE);
+            pos.x += width / strlen(kidiot->second_floor[x]) / 2;
         }
-        pos_s.x = (wd / 2);
-        pos_s.y += hg / get_map_size(play->second_floor);
+        pos.x = (width / 2);
+        pos.y += height / get_map_size(kidiot->second_floor);
     }
-    if (play->baby->floor == 1) {
-        baby_play.x += wd / 2;
-        draw_baby(play, baby_play);
+    pos.y = 0;
+    for (size_t x  = 0; kidiot->second_floor[x] != NULL; x++) {
+        for (size_t y = 0; kidiot->second_floor[x][y] != '\0'; y++) {
+            my_draw_color(kidiot, kidiot->second_floor[x][y], pos, size);
+            pos.x += width / strlen(kidiot->second_floor[x]) / 2;
+        }
+        pos.x = (width / 2);
+        pos.y += height / get_map_size(kidiot->second_floor);
     }
-    if (play->mom->floor == 1 && play->baby->vacuum->time <= 0) {
-        mom_play.x += wd / 2;
-        draw_mom(play, mom_play);
+    if (kidiot->baby->floor == 1) {
+        baby.x += width / 2;
+        draw_baby(kidiot, baby);
+    }
+    if (kidiot->mom->floor == 1 && kidiot->baby->vacuum->time <= 0) {
+        mom.x += width / 2;
+        draw_mom(kidiot, mom);
     }
 }
 
-void draw_map(kidiot_t *play, int hg, int wd)
+void draw_map(kidiot_t *kidiot, int height, int width)
 {
     Vector2 pos = {0, 0};
-    Vector2 size_rect = {wd / strlen(play->first_floor[0]) / 2, hg / get_map_size(play->first_floor)};
-    Vector2 baby_play = {play->baby->pos.y * (wd / strlen(play->first_floor[0]) / 2),
-        play->baby->pos.x * (hg / get_map_size(play->first_floor))};
-    Vector2 mom_play = {play->mom->pos.y * (wd / strlen(play->first_floor[0]) / 2),
-        play->mom->pos.x * (hg / get_map_size(play->first_floor))};
-
-    for (size_t x = 0; play->first_floor[x] != NULL; x++) {
-        for (size_t y = 0; play->first_floor[x][y] != '\0'; y++) {
-            my_draw_color(play->first_floor[x][y], pos, size_rect, play->textures);
-            pos.x += wd / strlen(play->first_floor[x]) / 2;
+    Vector2 size = {width / strlen(kidiot->first_floor[0]) / 2, height / get_map_size(kidiot->first_floor)};
+    Vector2 baby = {kidiot->baby->pos.y * (width / strlen(kidiot->first_floor[0]) / 2),
+        kidiot->baby->pos.x * (height / get_map_size(kidiot->first_floor))};
+    Vector2 mom = {kidiot->mom->pos.y * (width / strlen(kidiot->first_floor[0]) / 2),
+        kidiot->mom->pos.x * (height / get_map_size(kidiot->first_floor))};
+    
+    for (size_t x = 0; kidiot->first_floor[x] != NULL; x++) {
+        for (size_t y = 0; kidiot->first_floor[x][y] != '\0'; y++) {
+            DrawTexturePro(kidiot->textures[2], (Rectangle){877, 0, 73, 58},
+            (Rectangle){pos.x, pos.y, size.x, size.y}, (Vector2){0, 0}, 0, RAYWHITE);
+            pos.x += width / strlen(kidiot->first_floor[x]) / 2;
         }
         pos.x = 0;
-        pos.y += hg / get_map_size(play->first_floor);
+        pos.y += height / get_map_size(kidiot->first_floor);
     }
-    draw_second_map(play, wd, hg);
-    if (play->baby->floor == 0)
-        draw_baby(play, baby_play);
-    if (play->mom->floor == 0 && play->baby->vacuum->time <= 0)
-        draw_mom(play, mom_play);
+    pos.y = 0;
+    for (size_t x = 0; kidiot->first_floor[x] != NULL; x++) {
+        for (size_t y = 0; kidiot->first_floor[x][y] != '\0'; y++) {
+            my_draw_color(kidiot, kidiot->first_floor[x][y], pos, size);
+            pos.x += width / strlen(kidiot->first_floor[x]) / 2;
+        }
+        pos.x = 0;
+        pos.y += height / get_map_size(kidiot->first_floor);
+    }
+    draw_second_map(kidiot, width, height);
+    if (kidiot->baby->floor == 0)
+        draw_baby(kidiot, baby);
+    if (kidiot->mom->floor == 0 && kidiot->baby->vacuum->time <= 0)
+        draw_mom(kidiot, mom);
 }
